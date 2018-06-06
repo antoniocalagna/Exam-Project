04662b15395720d397c5f03a4bb30f97438015f3
Alex <ale.scisca2@gmail.com>
2018-06-06 20:39:16 +0200

    Continuazione di FileHandler e creazione della cartella Files

--- Exam Project/Libraries/Headers/FileHandler.h
+++ Exam Project/Libraries/Headers/FileHandler.h
@@ -5,8 +5,16 @@
 #ifndef SOCIAL_NETWORK_FILEHANDLER_H
 #define SOCIAL_NETWORK_FILEHANDLER_H
 
-namespace fh {    //File handler
+#include <string>
+#include <fstream>
 
-}
+namespace fh { //File handler
+
+//Ritorna il contenuto di una stringa
+std::string getScopeContent(const std::string &scope);
+
+int checkAccountsFile(std::ifstream &f);
+
+} //Namespace fh
 
 #endif //SOCIAL_NETWORK_FILEHANDLER_H
--- Exam Project/Libraries/Sources/FileHandler.cpp
+++ Exam Project/Libraries/Sources/FileHandler.cpp
@@ -2,3 +2,43 @@
 // Created by Alex on 06/06/2018.
 //
 
+#include "FileHandler.h"
+#include <iostream>
+
+std::string fh::getScopeContent(const std::string &scope) {
+  std::string out = "";
+  if (scope[0] != '{' || scope[scope.size() - 1] != '}')
+    return out;
+  return scope.substr(1, scope.size() - 2);
+}
+
+int fh::checkAccountsFile(std::ifstream &f) {
+  //Controlla se il file è aperto
+  if (!f.is_open()) {
+    return 0xFFFFFFFF;
+  }
+  
+  //Riporta il cursore di get all'inizio e controlla che il file sia leggibile
+  f.seekg(0);
+  if (!f.good()) {
+    return 0xFFFFFFFF;
+  }
+  
+  while (f.good()) {
+    //Acquisisci l'ID
+    std::string id;
+    std::getline(f, id, ',');         //L'ID non contiene virgole
+    if (!f.good()) {
+      //Problema di formato
+      return 0x00000001;
+    }
+    
+    //Acquisici il tipo di account
+    std::string type;
+    std::getline(f, type, ',');
+    if (!f.good()) {
+      //Problema di formato
+      return 0x00000001;
+    }
+  }
+}
