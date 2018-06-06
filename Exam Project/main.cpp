a08f137f05c69c56bd64e3e0bad306368fee3ff4
Antonio Calagna <antonio.calagna@gmail.com>
2018-06-06 17:27:19 +0200

    Initial Commit

---
+++ Exam Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Exam Project
+//
+//  Created by Antonio Calagna on 06/06/18.
+//  Copyright © 2018 Calagna-Scisca-Salvalai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
