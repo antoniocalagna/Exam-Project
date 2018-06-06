ed31092ae82440c734cf51c237588c9e9b92fc70
Antonio Calagna <antonio.calagna@gmail.com>
2018-06-06 18:30:19 +0200

    Indentazione

--- Exam Project/Libraries/Headers/Post.h
+++ Exam Project/Libraries/Headers/Post.h
@@ -14,54 +14,54 @@ using namespace std;
 
 class Post {
 public:
-    //Constructors & Destructor
-    Post();
-    Post(const string &iduser, const string &news, const Clock &time, const vector<string> &likes,
-         const vector<string> &dislikes);
-    ~Post();
-
-    //Setters
-    void setNews(const string &news);
-    void setIdUser(const string &iduser);
-    void setTime(const Clock &time);
-    void setLike(const vector<string> &likes);
-    void setDislike(const vector<string> &dislikes);
-
-    //Getters
-    string getNews() const;
-    string getIdUser() const;
-    Clock getTime() const;
-    vector<string> getLikes() const;
-    vector<string> getDislikes() const;
-
-    //Overloading operators
-    bool operator==(const Post &to_be_compared);//non comparo i likes perchè mi sembrano parametri troppo variabili
-    bool operator!=(const Post &to_be_compared);
-    bool operator>(const Post &is_smaller); //potrebbero basarsi sul numero di likes
-    bool operator<(const Post &is_greater);
-    friend ostream &operator<<(ostream &stream, const Post &p);
-
-    //Others
-    void AddLike(const string &id);
-    void AddDislike(const string &id);
-    void RemoveLike(const string &id);
-    void RemoveDislike(const string &id);
-    int SearchLike(const string &ID) const; //passo un ID, ritorna l'indice del vettore se il likes c'è, sennò ritorna -1
-    int SearchDislike(const string &ID)const;
-    int NumLikes(); //numero likes
-    int NumDislikes();
-    float RatioReaction(); //rapporto LIKE/(DISLIKE+LIKE) così da poter studiare i post più 'popolari'
-
-
+  //Constructors & Destructor
+  Post();
+  Post(const string &iduser, const string &news, const Clock &time, const vector<string> &likes,
+       const vector<string> &dislikes);
+  ~Post();
+  
+  //Setters
+  void setNews(const string &news);
+  void setIdUser(const string &iduser);
+  void setTime(const Clock &time);
+  void setLike(const vector<string> &likes);
+  void setDislike(const vector<string> &dislikes);
+  
+  //Getters
+  string getNews() const;
+  string getIdUser() const;
+  Clock getTime() const;
+  vector<string> getLikes() const;
+  vector<string> getDislikes() const;
+  
+  //Overloading operators
+  bool operator==(const Post &to_be_compared);//non comparo i likes perchè mi sembrano parametri troppo variabili
+  bool operator!=(const Post &to_be_compared);
+  bool operator>(const Post &is_smaller); //potrebbero basarsi sul numero di likes
+  bool operator<(const Post &is_greater);
+  friend ostream &operator<<(ostream &stream, const Post &p);
+  
+  //Others
+  void AddLike(const string &id);
+  void AddDislike(const string &id);
+  void RemoveLike(const string &id);
+  void RemoveDislike(const string &id);
+  int SearchLike(const string &ID) const; //passo un ID, ritorna l'indice del vettore se il likes c'è, sennò ritorna -1
+  int SearchDislike(const string &ID)const;
+  int NumLikes(); //numero likes
+  int NumDislikes();
+  float RatioReaction(); //rapporto LIKE/(DISLIKE+LIKE) così da poter studiare i post più 'popolari'
+  
+  
 private:
-    string _news;
-    string _iduser;
-    Clock _time;
-    vector<string> _likes, _dislikes; //empty constructors
-
-    //ho deciso di usare VETTORI e non LISTE, perchè così posso accedervi tramite indice, senza dover sfogliare dall'inizio
-    //(((((me l'ha detto Scisca, se non è vero ha sbagliato lui.))))) :-) tvb
-
+  string _news;
+  string _iduser;
+  Clock _time;
+  vector<string> _likes, _dislikes; //empty constructors
+  
+  //ho deciso di usare VETTORI e non LISTE, perchè così posso accedervi tramite indice, senza dover sfogliare dall'inizio
+  //(((((me l'ha detto Scisca, se non è vero ha sbagliato lui.))))) :-) tvb
+  
 };
 
 
