f4695b9d3a54f6fcb6cb068875436816390ae220
Alessandro Scisca <ale.scisca2@gmail.com>
2018-06-08 14:04:09 +0200

    Continuazione del FileHandler

--- Exam Project/Libraries/Sources/FileHandler.cpp
+++ Exam Project/Libraries/Sources/FileHandler.cpp
@@ -5,11 +5,36 @@
 #include "FileHandler.h"
 #include <iostream>
 
-std::string fh::getScopeContent(const std::string &scope) {
-  std::string out = "";
-  if (scope[0] != '{' || scope[scope.size() - 1] != '}')
-    return out;
-  return scope.substr(1, scope.size() - 2);
+std::string fh::error(int code) {
+  std::string err;
+  switch (code) {
+    case 0:
+      err = "File formattato correttamente.";
+      break;
+    case 0xFFFFFFFF:
+      err = "File non aperto o danneggiato.";
+      break;
+    case 0x00000001:
+      err = "Errore di formato.";
+      break;
+    default:
+      err = "Errore " + std::to_string(code) + " sconosciuto.";
+      break;
+  }
+  return err;
+}
+
+std::string fh::readField(const std::string &field_name, const std::string &raw_str) {
+  size_t field_pos = raw_str.find(field_name);  //Ricerca del campo
+  if (field_pos == std::string::npos) {          //Il campo non è presente
+    return "";
+  }
+  if (field_pos != 0) {                          //La stringa contiene altri dati. Per scelta non devono esserci.
+    return "";
+  }
+  
+  //Non considerare il nome del campo, i : e le parentesi graffe
+  return raw_str.substr(field_name.size() + 2, raw_str.size() - (field_name.size() + 3));
 }
 
 int fh::checkAccountsFile(std::ifstream &f) {
@@ -21,24 +46,23 @@ int fh::checkAccountsFile(std::ifstream &f) {
   //Riporta il cursore di get all'inizio e controlla che il file sia leggibile
   f.seekg(0);
   if (!f.good()) {
-    return 0xFFFFFFFF;
+    return 0xFFFFFFFF;                                          //Errore nel file
   }
   
   while (f.good()) {
     //Acquisisci l'ID
     std::string id;
-    std::getline(f, id, ',');         //L'ID non contiene virgole
-    if (!f.good()) {
-      //Problema di formato
-      return 0x00000001;
-    }
+    std::getline(f, id, ',');                                   //L'ID non contiene virgole
+    if (!f.good()) { return 0x00000001; }
     
     //Acquisici il tipo di account
     std::string type;
     std::getline(f, type, ',');
-    if (!f.good()) {
-      //Problema di formato
-      return 0x00000001;
-    }
+    if (!f.good()) { return 0x00000001; }                       //Problema di formato
+    
+    //Raccogli tutte le informazioni sull'utente
+    std::string info;
+    std::getline(f, info, '}');     //Qui bisogna fermarsi alla graffa
+    if (!f.good()) { return 0x00000001; }
   }
 }
